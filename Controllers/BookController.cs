f8edc61c46e201f8e90a9dc7e12f80929439fccc
Luis Guilherme <luislebert01@outlook.com>
2023-04-13 10:11:35 -0300

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+
+namespace Controllers
+{
+    public class BookController: Book
+    {
+        public void InsertBook(int id, string title, string author)
+        {
+            Book book = new Book();
+            book.Id = id;
+            book.Title = title;
+            book.Author = author;
+        }
+    }
+}
